a60c1a5ecfce454f6c62915019d7af32973173fa
Dc Valerian <Dc-Valerian>
2024-03-09 11:15:31 +0100

    added the reducers and store file

---
+++ src/Services/reducers.tsx
@@ -0,0 +1,18 @@
+// WRITING PURE JAVASCRIPT FUNCTIONS
+
+
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+
+}
+
+const reducers = createSlice({
+  name: second,
+  initialState,
+  reducers: {}
+});
+
+export const {} = reducers.actions
+
+export default reducers.reducer
